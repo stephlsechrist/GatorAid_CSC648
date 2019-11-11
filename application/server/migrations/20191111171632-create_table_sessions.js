16ebd59dd59bb557c1a0466be61202c598acd9eb
Ali Nasralla <amnasralla@gmail.com>
2019-11-11 09:34:16 -0800

    added tables

--- application/server/migrations/20191111171632-create_table_sessions.js
+++ application/server/migrations/20191111171632-create_table_sessions.js
@@ -2,22 +2,24 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('session', {
+      sid: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+      },
+      sess: {
+        type: Sequelize.JSON,
+        allowNull: false
+      },
+      expire: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('session')
   }
 };
