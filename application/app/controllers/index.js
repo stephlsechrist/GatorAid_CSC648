8ee6c7ca727fb318af176d85a637e5365095b4a6
Jackie665 <34040434+Jackie665@users.noreply.github.com>
2019-12-11 15:52:56 -0600

    Added router.get to About us Page

--- application/app/controllers/index.js
+++ application/app/controllers/index.js
@@ -14,6 +14,37 @@ module.exports = function (router) {
     });
   });
 
+  router.get('/about-us', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'about-us'));
+  });
+
+  router.get('/contact-us', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'contact-us'));
+  });
+
+  router.get('/Steph', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'Steph'));
+  });
+
+  router.get('/Nikhil', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'Nikhil'));
+  });
+
+  router.get('/Nayan', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'Nayan'));
+  });
+
+  router.get('/Jackie', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'Jackie'));
+  });
+
+  router.get('/George', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'George'));
+  });
+
+  router.get('/Ali', function(req, res) {
+    res.render(path.join(__dirname, '../../views', 'Ali'));
+  });
 
   router.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../public', 'index.html'));
